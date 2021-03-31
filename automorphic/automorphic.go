23b1b813702af116bbacde63d1dc6052cab55583
Dmitry Vesnin <dvesnin@exadel.com>
2021-03-31 23:10:25 +0500

    Task 1: added better implementation

--- automorphic/automorphic.go
+++ automorphic/automorphic.go
@@ -1,10 +1,25 @@
 package automorphic
 
 import (
+	"math"
 	"strconv"
 )
 
+// Two different implementations, because why not
 func Automorphic(n uint64) bool {
+	return automorphicMath(n)
+}
+
+func automorphicMath(n uint64) bool {
+	if n == 0 {
+		return true
+	}
+	sq := n * n
+	pow := int(math.Floor(math.Log10(float64(n)))) + 1
+	return sq%uint64(math.Pow10(pow)) == n
+}
+
+func automorphicStrings(n uint64) bool {
 	sq := n * n
 	nString, sqString := strconv.FormatUint(n, 10), strconv.FormatUint(sq, 10)
 	nSize, sqSize := len(nString), len(sqString)
